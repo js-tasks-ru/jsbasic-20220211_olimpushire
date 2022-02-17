d20792280ce9722090391d8f069da9433c7cde27
Anton Maksymenko <olimpushire@gmail.com>
2022-02-17 21:06:54 +0100

    1-Module/4-Task - добавлена функция проверки на спам.

--- 1-module/4-task/index.js
+++ 1-module/4-task/index.js
@@ -1,3 +1,17 @@
 function checkSpam(str) {
   // ваш код...
+
+  //Напишите функцию `checkSpam(str)`, возвращающую `true`, если `str` содержит `'1xBet'` или `'XXX'`, а иначе `false`.
+  const spamWord1 = '1xBet'.toLowerCase();
+  const spamWord2 = 'XXX'.toLowerCase();
+
+  str = str.toLowerCase();
+
+  if (str.includes(spamWord1) || str.includes(spamWord2)) {
+    return true;
+  } else {
+    return false;
+  }
+
+
 }
