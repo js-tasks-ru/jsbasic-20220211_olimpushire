4987022639903c672456a275cd6e31ee27f1ed68
Anton Maksymenko <olimpushire@gmail.com>
2022-02-17 12:19:06 +0100

    Added Function factorial

--- 1-module/1-task/index.js
+++ 1-module/1-task/index.js
@@ -1,3 +1,15 @@
 function factorial(n) {
-  // ваш код...
+	let nResult = n;
+	let decrNumb = 1;
+
+	if (n === 0 || n === 1) {
+		return 1;
+	}
+
+	do {
+		nResult *= n - decrNumb;
+		decrNumb = decrNumb + 1;	
+	} while ((n - decrNumb) > 0 );
+	  
+	return nResult;
 }
