f5b5c2cb042727be3bfc5bfa11b8d069c5ec4886
Anton Maksymenko <olimpushire@gmail.com>
2022-02-19 00:07:36 +0100

    Tasks from Module #2. 1-Task

--- 2-module/1-task/index.js
+++ 2-module/1-task/index.js
@@ -1,3 +1,11 @@
 function sumSalary(salaries) {
   // ваш код...
+  let salSum = 0;
+	for (const key in salaries) {
+		if (isFinite(salaries[key])) {
+			salSum += salaries[key];
+		}		
+	}
+	
+	return salSum;
 }
